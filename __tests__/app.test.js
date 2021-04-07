f27af718c1a91b385964b3e763f28349976b23df
Cierra-McDonald <cierralauren15@gmail.com>
2021-04-06 22:58:29 -0700

    all dog routes, tests, postman passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('lab-08-CRRUD-build routes', () => {
+describe('lab-08-CRRUD-build CAT routes', () => {
   beforeAll(() => {
     return setup(pool);
   });
@@ -70,3 +70,72 @@ describe('lab-08-CRRUD-build routes', () => {
   
   });
 
+
+  describe('lab-08-CRRUD-build DOG routes', () => {
+    beforeAll(() => {
+      return setup(pool);
+    });
+    it('creates a dog and updates in the cats database', () => { 
+      return request(app)
+        .post('/api/v1/dogs')
+        .send({ dog_name: 'Smokey', breed: 'Black Lab'})
+        .then((res) => { 
+          expect(res.body).toEqual({
+            id: '1',
+            dog_name: 'Smokey',
+            breed: 'Black Lab'
+          });
+        });
+  
+    });
+    it('gets a dog by the id', async () => { 
+      const response = await request(app)
+        .get('/api/v1/dogs/1')
+  
+        expect(response.body).toEqual({ 
+          id: '1',
+          dog_name: 'Smokey',
+          breed: 'Black Lab'
+        });
+    })
+    it('gets all dogs', async () => { 
+      const response = await request(app)
+        .get('/api/v1/dogs')
+  
+        expect(response.body).toEqual([
+          {
+            id: '1',
+            dog_name: 'Smokey',
+            breed: 'Black Lab'
+          }
+        ]);
+    })
+    
+    it('should update a dog by id', async () => { 
+      await request(app)
+      .post('/api/v1/dogs')
+      .send({dog_name: 'Smokey', breed: 'Black Lab'})
+      
+      await request(app)
+      .put('/api/v1/dogs/1')
+      .send({ dog_name: 'Smokerss', breed: 'Black Lab'})
+      
+      const result = await request(app)
+      .get('/api/v1/dogs/1')
+      console.log(result.body);
+      expect(result.body).toEqual({ 
+        id: '1',
+        dog_name: 'Smokerss',
+        breed: 'Black Lab'
+      })
+    })
+
+    it('gets a dog by id and deletes from the database', async () => { 
+      const response = await request(app)
+        .delete('/api/v1/dogs/1')
+  
+        expect(response.body).toEqual({})
+    })
+    
+    });
+  
